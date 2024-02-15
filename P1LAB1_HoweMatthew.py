d11408ce583eb648d946724b4c2b63a8b3c75319
SmithM457 <160173143+SmithM457@users.noreply.github.com>
2024-02-15 14:36:29 -0500

    Add files via upload

---
+++ P1LAB1_HoweMatthew.py
@@ -0,0 +1,17 @@
+# Matthew Howe
+# 2/15/24
+# P1LAB1
+# This program will display my name and a welcome message
+
+### Get the first name of the user and assign it to a variable
+##fname = input("Enter your first name: ")
+### Get the first name of the user and assign it to a variable
+##lname = input("Enter your last name: ")
+### Print with welcome message
+##print("Hello," ,fname, lname + "! Welcome to CTI-110")
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+# Write a math statement
+num3 = num1 + num2
+print(str(num1) + ' + ' + str(num2) + ' = ' + (str(num3)))
