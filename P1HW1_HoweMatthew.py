298e5b12cf89f35551810d4005b5ec62d255ca2c
SmithM457 <160173143+SmithM457@users.noreply.github.com>
2024-02-22 13:57:08 -0500

    Add files via upload

---
+++ P1HW1_HoweMatthew.py
@@ -0,0 +1,17 @@
+#Matthew Howe
+#2/22/24
+#P1HW1
+#Creating a processing output for integers
+
+
+num1 = int(input("Enter an integer: "))
+print("You entered", num1)
+num1sq = num1 * num1
+print(num1, "squard is", num1sq)
+num1cu = num1sq * num1
+print("And", num1, "squared is", num1cu, "!!")
+num2 = int(input("Enter another integer: "))
+num3 = num1 + num2
+num4 = num1 * num2
+print(num1, "+", num2, "is", num3)
+print(num1, "*", num2, "is", num4)
